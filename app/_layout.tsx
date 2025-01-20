52a4bf7deb727f805b3eabe65a2df959d0dcea23
Caiqui Viera <cvcommunitymanager@gmail.com>
2025-01-20 17:44:23 -0300

    commit inicial

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+import { View } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <View style={{ flex: 1 }}>
+      {/* Para la maayoria de casos de uso Stack es mejor, porque ya tiene cierta estructura
+          predefinida para volver hacia atras, animaciones como las nativas, etc */}
+      <Stack
+      // screenOptions={{
+      //   headerTitle: "",
+      //   headerLeft: () => <Logo />,
+      //   headerRight: () => {
+      //     return (
+      //       <Link asChild href="/info">
+      //         <Pressable>
+      //           <InfoIcon size={40} color="black" />
+      //         </Pressable>
+      //       </Link>
+      //     );
+      //   },
+      // }}
+      />
+      {/* <Slot /> -> se usa para mayor personalizacion entre las transiciones y demas*/}
+    </View>
+  );
+}
