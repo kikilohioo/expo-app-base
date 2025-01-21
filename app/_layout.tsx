e03a0840e9c799b2f583ff3fe2f9b7364e2a2972
Caiqui Viera <cvcommunitymanager@gmail.com>
2025-01-21 18:59:23 -0300

    agregamos conexion basica con sqlite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,53 @@
 import { Stack } from "expo-router";
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
+import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+const DB_NAME = process.env.EXPO_PUBLIC_DB_NAME;
+
+const createDbIfNeeded = async (db: SQLiteDatabase) => {
+  //
+  console.log("Creating database");
+  try {
+    // Create a table and edit for create a migration way to create multiple tables
+    const response = await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, image TEXT)",
+    );
+    console.log("Database created", response);
+  } catch (error) {
+    console.error("Error creating database:", error);
+  }
+};
 
 export default function RootLayout() {
   return (
     <View style={{ flex: 1 }}>
-      {/* Para la maayoria de casos de uso Stack es mejor, porque ya tiene cierta estructura
-          predefinida para volver hacia atras, animaciones como las nativas, etc */}
-      <Stack
-        screenOptions={{
-          headerTitle: "", // aca podria ir el titulo
-          // headerLeft: () => <Logo />,
-          // headerRight: () => {
-          //   return (
-          //     <Link asChild href="/info">
-          //       <Pressable>
-          //         <InfoIcon size={40} color="black" />
-          //       </Pressable>
-          //     </Link>
-          //   );
-          // },
-        }}
-      />
-      {/* <Slot /> -> se usa para mayor personalizacion entre las transiciones y demas*/}
+      <SQLiteProvider databaseName={DB_NAME} onInit={createDbIfNeeded}>
+        <Stack
+          screenOptions={{
+            headerTitle: "", // aca podria ir el titulo
+            // headerLeft: () => <Logo />,
+            // headerRight: () => {
+            //   return (
+            //     <Link asChild href="/info">
+            //       <Pressable>
+            //         <InfoIcon size={40} color="black" />
+            //       </Pressable>
+            //     </Link>
+            //   );
+            // },
+          }}
+        >
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen
+            name="modal"
+            options={{
+              presentation: "modal",
+            }}
+          />
+        </Stack>
+      </SQLiteProvider>
+      <StatusBar style="auto" />
     </View>
   );
 }
